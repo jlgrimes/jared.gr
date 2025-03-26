daaa259cd2bee969e1ca23e72f25f432d85fced8
Jared Grimes <jaredlancegrimes@gmail.com>
2025-03-26 00:04:12 -0500

    added viewport meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from "next";
-import { Geist } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Jared Grimes",
-  description: "Personal website of Jared Grimes",
+  title: 'Jared Grimes',
+  description: 'Personal website of Jared Grimes',
 };
 
 export default function RootLayout({
@@ -18,7 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </head>
       <body className={`${geistSans.variable} antialiased`}>{children}</body>
     </html>
   );
