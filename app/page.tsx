e7fe71c699db6a8bcdbe79451c6fc984d165a731
Jared Grimes <jaredlancegrimes@gmail.com>
2025-03-26 00:11:00 -0500

    can this plz fix mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Chat from "./components/chat/index";
+import Chat from './components/chat/index';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900">
-      <div className="container mx-auto px-4 py-8 h-screen flex flex-col">
-        <div className="max-w-2xl mx-auto w-full flex flex-col h-[calc(100vh-4rem)]">
+    <div className='bg-white dark:bg-gray-900 h-full'>
+      <div className='container mx-auto px-4 py-8 flex flex-col h-full'>
+        <div className='max-w-2xl mx-auto w-full flex flex-col h-full'>
           <Chat />
         </div>
       </div>
