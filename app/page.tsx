4a6a6ac11d292a977744e21f0a7f23ecc52275b2
Jared Grimes <jaredlancegrimes@gmail.com>
2025-05-15 12:14:28 -0500

    removed language selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { Hero } from './components/Hero';
 import { Projects } from './components/projects/Projects';
-import { LanguageSelector } from './components/LanguageSelector';
 import { I18nProvider } from './components/I18nProvider';
 
 export default function Home() {
   return (
     <I18nProvider>
       <div className='bg-white dark:bg-gray-900 min-h-screen'>
-        <LanguageSelector />
+        {/* <LanguageSelector /> */}
         <div className='container mx-auto px-4 py-8'>
           <div className='max-w-2xl mx-auto w-full'>
             <Hero />
