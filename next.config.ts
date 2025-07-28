370510db71d4c5c8b8bb45e3432755446e82a6a3
Jared Grimes <jaredlancegrimes@gmail.com>
2025-07-28 18:51:48 -0500

    redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,65 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'http://discordapp.com/users/265515383773986817',
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/jaredlgrimes',
+        permanent: true,
+      },
+      {
+        source: '/in',
+        destination: 'https://linkedin.com/in/jaredlgrimes',
+        permanent: true,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://youtube.com/@jgrimesey',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://x.com/jgrimesey',
+        permanent: true,
+      },
+      {
+        source: '/x',
+        destination: 'https://x.com/jgrimesey',
+        permanent: true,
+      },
+      {
+        source: '/insta',
+        destination: 'https://instagram.com/jaredgrimesey',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/jaredgrimesey',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/jlgrimes',
+        permanent: true,
+      },
+      {
+        source: '/gh',
+        destination: 'https://github.com/jlgrimes',
+        permanent: true,
+      },
+      {
+        source: '/limitless',
+        destination: 'https://limitlesstcg.com/players/450',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
