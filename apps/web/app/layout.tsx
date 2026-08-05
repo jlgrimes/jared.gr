81bcca4268be2a3f5ab32a5c553020edf88fad44
Jared Grimes <jaredlancegrimes@gmail.com>
2026-08-05 16:54:32 -0500

    mobile fix

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,6 +8,11 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: '#ffffeb',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
 };
 
 export default function RootLayout({
@@ -19,7 +24,7 @@ export default function RootLayout({
     <html lang='en' style={{ backgroundColor: '#ffffeb' }}>
       <head>
         <meta name='theme-color' content='#ffffeb' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover' />
       </head>
       <body className='font-sans antialiased overflow-hidden' style={{ backgroundColor: '#ffffeb' }}>
         {children}
